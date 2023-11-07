aefe59b1a13c7505d7d0c1ebe99024530886717b
Prajakta Dhamale <142687977+PD2601@users.noreply.github.com>
2023-11-07 19:59:27 +0530

    Add files via upload

---
+++ Lab_Assessment_2/src/com/Transaction/Targets/DriverApp.java
@@ -0,0 +1,46 @@
+package com.Transaction.Targets;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DriverApp {
+	public static void main(String[] args) {
+		int days;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter number of days shop was open: ");
+		days = sc.nextInt();
+
+		int trans[] = new int[days];
+		for (int i = 0; i < trans.length; i++) {
+			System.out.print("Please enter income of Day " + (i + 1) + " : ");
+			trans[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(trans));
+
+		System.out.println("Please enter number of targets: ");
+		int targets = sc.nextInt();
+
+		int sum;
+		int flag;
+
+		for (int j = 1; j <= targets; j++) {
+			System.out.println("Please enter value for target: " + j);
+			int singleTarget = sc.nextInt();
+			flag = 0;
+			sum = 0;
+
+			for (int k = 0; k < trans.length; k++) {
+				sum = sum + trans[k];
+				if (sum >= singleTarget) {
+					flag = 1;
+					System.out.println("Target achieved on day " + (k + 1));
+					break;
+				}
+			}
+			if (flag == 0) {
+				System.out.println("Target NOT achieved");
+			}
+		}
+		sc.close();
+	}
+}
